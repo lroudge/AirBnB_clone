adfd641bb1d709dcc488ce4c720836dd38ad24dd
lroudge <lauradrc@hotmail.fr>
2019-06-27 21:52:09 +0000

    Add tests for task 3

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             
     def __str__(self):
         """Returns a human-readable string representation
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,6 +2,8 @@
 """Unittest module for the BaseModel Class."""
 
 import unittest
+from datetime import datetime
+import time
 from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
@@ -20,28 +22,56 @@ class TestBaseModel(unittest.TestCase):
 
         b = BaseModel()
         self.assertEqual(str(type(b)), "")
-        self.assertTrue(isinstance(b, BaseModel))
+        self.assertIsInstance(b, BaseModel)
         self.assertFalse(issubclass(b, BaseModel))
 
     def test_3_attributes(self):
         """Tests attributes value for instance of a BaseModel class."""
 
+        date_now = datetime.now()
         b = BaseModel()
-        """How to test for random values like id and dates?"""
+        diff = b.updated_at - b.created_at
+        self.assertTrue(diff.total_seconds() < abs(0.01))
+        diff = b.created_at - date_now
+        self.assertTrue(diff.total_seconds() < abs(0.1))
+
+    def test_3_id(self):
+        """Tests for unique user ids."""
+
+        l = []
+        for i in range(1000):
+            b = BaseModel()
+            l.append(b.id)
+        self.assertEqual(len(set(l)), len(l))
 
     def test_3_save(self):
         """Tests the public instance method save()."""
         
         b = Base()
+        time.sleep(0.5)
+        date_now = datetime.now()
         b.save()
-        """How to test for random date?"""
+        diff = b.updated_at - date_now
+        self.assertTrue(diff.total_seconds() < abs(0.01))
+
+    def test_3_str(self):
+        """Tests for __str__ method."""
+
+        
 
     def test_3_to_dict(self):
         """Tests the public instance method to_dict()."""
         
         b = Base()
+        b.name = "Laura"
+        b.age = 23
         d = b.to_dict()
-        """How to test for random values like id and dates?"""
+        self.assertEqual(d["id"], b.id)
+        self.assertEqual(d["__class__"], type(b).__name__)
+        self.assertEqual(d["created_at"], b.created_at.isoformat())
+        self.assertEqual(d["updated_at"], b.updated_at.isoformat())
+        self.assertEqual(d["name"], b.name)
+        self.assertEqual(d["age"], b.age)
 
     def test_4_instantiation(self):
         """Tests instantiation with **kwargs."""
