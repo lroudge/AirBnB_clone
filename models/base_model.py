e88f9a7321045314e126bc27bd83024f44756fba
lroudge <lauradrc@hotmail.fr>
2019-06-27 18:20:51 +0000

    Add BaseModel task 3

---
+++ models/base_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Module for Base class
+Contains the Base class for the AirBnB clone console.
+"""
+
+import uuid
+import datetime
+
+class BaseModel:
+    """Class for base model of object hierarchy."""
+
+    def __init__(self):
+        """Initialization of a Base instance."""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Returns a human-readable string representation
+        of an instance."""
+
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the updated_at attribute
+        with the current datetime."""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary representation of an instance."""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Unittest module for the BaseModel Class."""
+
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    """Test Cases for the BaseModel class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        pass
+    
+    def test_3_instantiation(self):
+        """Tests instantiation of BaseModel class."""
+
+        b = BaseModel()
+        self.assertEqual(str(type(b)), "")
+        self.assertTrue(isinstance(b, BaseModel))
+        self.assertFalse(issubclass(b, BaseModel))
+
+    def test_3_attributes(self):
+        """Tests attributes value for instance of a BaseModel class."""
+
+        b = BaseModel()
+        """How to test for random values like id and dates?"""
+
+    def test_3_save(self):
+        """Tests the public instance method save()."""
+        
+        b = Base()
+        b.save()
+        """How to test for random date?"""
+
+    def test_3_to_dict(self):
+        """Tests the public instance method to_dict()."""
+        
+        b = Base()
+        d = b.to_dict()
+        """How to test for random values like id and dates?"""
