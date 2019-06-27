d0c21ea0eca676fbf0f1022185b1e19d721d03e5
Arthur Damm <indigoarthur@gmail.com>
2019-06-27 23:29:48 +0000

    Add unit tests for BaseModel instantiation and __str__

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,6 +5,8 @@ import unittest
 from datetime import datetime
 import time
 from models.base_model import BaseModel
+import re
+import json
 
 class TestBaseModel(unittest.TestCase):
     """Test Cases for the BaseModel class."""
@@ -21,9 +23,9 @@ class TestBaseModel(unittest.TestCase):
         """Tests instantiation of BaseModel class."""
 
         b = BaseModel()
-        self.assertEqual(str(type(b)), "")
+        self.assertEqual(str(type(b)), "<class 'models.base_model.BaseModel'>")
         self.assertIsInstance(b, BaseModel)
-        self.assertFalse(issubclass(b, BaseModel))
+        self.assertTrue(issubclass(type(b), BaseModel))
 
     def test_3_attributes(self):
         """Tests attributes value for instance of a BaseModel class."""
@@ -47,7 +49,7 @@ class TestBaseModel(unittest.TestCase):
     def test_3_save(self):
         """Tests the public instance method save()."""
         
-        b = Base()
+        b = BaseModel()
         time.sleep(0.5)
         date_now = datetime.now()
         b.save()
@@ -56,13 +58,24 @@ class TestBaseModel(unittest.TestCase):
 
     def test_3_str(self):
         """Tests for __str__ method."""
-
-        
+        b = BaseModel()
+        rex = re.compile("^\[(.*)\] \((.*)\) (.*)$")
+        res = rex.match(str(b))
+        self.assertIsNotNone(res)
+        self.assertEqual(res.group(1), "BaseModel")
+        self.assertEqual(res.group(2), b.id)
+        s = res.group(3)
+        s = re.sub("(datetime\.datetime\([^)]*\))", "'\\1'", s)
+        d = json.loads(s.replace("'", '"'))
+        d2 = b.__dict__.copy()
+        d2["created_at"] = repr(d2["created_at"])
+        d2["updated_at"] = repr(d2["updated_at"])
+        self.assertEqual(d, d2)
 
     def test_3_to_dict(self):
         """Tests the public instance method to_dict()."""
         
-        b = Base()
+        b = BaseModel()
         b.name = "Laura"
         b.age = 23
         d = b.to_dict()
@@ -81,4 +94,4 @@ class TestBaseModel(unittest.TestCase):
         my_model.my_number = 89
         my_model_json = my_model.to_dict()
         my_new_model = BaseModel(**my_model_json)
-        """Test for dict equality?"""
+        self.assertEqual(my_new_model.to_dict(), my_model.to_dict())
