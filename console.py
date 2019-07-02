6c85f9c3e85623296cb77ac5e679d6a7e4f1f42c
lroudge <lauradrc@hotmail.fr>
2019-07-02 00:35:57 +0000

    Implement the .update() method in the console

--- console.py
+++ console.py
@@ -6,6 +6,7 @@ import inspect
 from models.base_model import BaseModel
 from models import storage
 import re
+import json
 
 
 class HBNBCommand(cmd.Cmd):
@@ -22,10 +23,50 @@ class HBNBCommand(cmd.Cmd):
         classname = match.group(1)
         method = match.group(2)
         args = match.group(3)
-        command = method + " " + classname + " " + args
+        match_uid_and_args = re.search('^"([^"]*)"(?:, (.*))?$', args)
+        if match_uid_and_args:
+            uid = match_uid_and_args.group(1)
+            attr_or_dict = match_uid_and_args.group(2)
+        else:
+            uid = args
+            attr_or_dict = False
+
+        attr_and_value = ""
+        if method == "update" and attr_or_dict:
+            match_dict = re.search('^({.*})$', attr_or_dict)
+            if match_dict:
+                self.update_dict(classname, uid, match_dict.group(1))
+                return ""
+            match_attr_and_value = re.search('^(?:"([^"]*)")?(?:, (.*))?$', attr_or_dict)
+            if match_attr_and_value:
+                attr_and_value = (match_attr_and_value.group(1) or "") + " " + (match_attr_value.group(2) or "")
+        command = method + " " + classname + " " + uid + " " + attr_and_value
         self.onecmd(command)
         return ""
 
+    def update_dict(self, classname, uid, s_dict):
+        """Helper method for update() with a dictionary."""
+        s = s_dict.replace("'", '"')
+        d = json.loads(s)
+        if not classname:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[classname]
+                for attribute, value in d.items():
+                    if attribute in attributes:
+                        value = attributes[attribute](value)
+                    setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+        
+        
     def do_EOF(self, line):
         """Handles EOF."""
         print()
@@ -143,9 +184,9 @@ class HBNBCommand(cmd.Cmd):
             key = "{}.{}".format(classname, uid)
             if key not in storage.all():
                 print("** no instance found **")
-            elif attribute is None:
+            elif not attribute:
                 print("** attribute name missing **")
-            elif value is None:
+            elif not value:
                 print("** value missing **")
             else:
                 value = value.replace('"', '')
