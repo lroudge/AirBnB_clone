2763ddc2159d5d182989ad00aa7834a18bd8858c
Arthur Damm <indigoarthur@gmail.com>
2019-07-03 23:18:16 +0000

    Refactor show, destroy tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -13,6 +13,7 @@ import os
 
 
 class TestHBNBCommand(unittest.TestCase):
+
     """Tests HBNBCommand console."""
 
     def setUp(self):
@@ -81,6 +82,190 @@ EOF  all  count  create  destroy  help  quit  show  update
         msg = f.getvalue()[:-1]
         self.assertEqual(msg, "** class doesn't exist **")
 
+    def test_do_show(self):
+        """Tests show for all classes."""
+        for classname in self.classes():
+            self.help_test_do_show(classname)
+            self.help_test_show_advanced(classname)
+
+    def help_test_do_show(self, classname):
+        """Helps test the show command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().
+                             onecmd("create {}".format(classname)))
+        uid = f.getvalue()[:-1]
+        self.assertTrue(len(uid) > 0)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(
+                HBNBCommand().onecmd("show {} {}".format(classname, uid)))
+        s = f.getvalue()[:-1]
+        self.assertTrue(uid in s)
+
+    def test_do_show_error(self):
+        """Tests show command with errors."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show garbage"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class doesn't exist **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show BaseModel"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("show BaseModel 6524359"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** no instance found **")
+
+    def help_test_show_advanced(self, classname):
+        """Helps test .show() command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().
+                             onecmd("create {}".format(classname)))
+        uid = f.getvalue()[:-1]
+        self.assertTrue(len(uid) > 0)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(
+                HBNBCommand().precmd(
+                    '{}.show("{}")'.format(
+                        classname,
+                        uid)))
+        s = f.getvalue()
+        self.assertTrue(uid in s)
+
+    def test_do_show_error_advanced(self):
+        """Tests show() command with errors."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd(".show()"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd("garbage.show()"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class doesn't exist **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd("BaseModel.show()"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd('BaseModel.show("6524359")'))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** no instance found **")
+
+    def test_do_destroy(self):
+        """Tests destroy for all classes."""
+        for classname in self.classes():
+            self.help_test_do_destroy(classname)
+            self.help_test_destroy_advanced(classname)
+
+    def help_test_do_destroy(self, classname):
+        """Helps test the destroy command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().
+                             onecmd("create {}".format(classname)))
+        uid = f.getvalue()[:-1]
+        self.assertTrue(len(uid) > 0)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(
+                HBNBCommand().onecmd(
+                    "destroy {} {}".format(
+                        classname,
+                        uid)))
+        s = f.getvalue()[:-1]
+        self.assertTrue(len(s) == 0)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd(".all()"))
+        self.assertFalse(uid in f.getvalue())
+
+    def test_do_destroy_error(self):
+        """Tests destroy command with errors."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy garbage"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class doesn't exist **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy BaseModel"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("destroy BaseModel 6524359"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** no instance found **")
+
+    def help_test_destroy_advanced(self, classname):
+        """Helps test the destroy command."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().
+                             onecmd("create {}".format(classname)))
+        uid = f.getvalue()[:-1]
+        self.assertTrue(len(uid) > 0)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(
+                HBNBCommand().precmd(
+                    '{}.destroy("{}")'.format(
+                        classname,
+                        uid)))
+        s = f.getvalue()[:-1]
+        self.assertTrue(len(s) == 0)
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd(".all()"))
+        self.assertFalse(uid in f.getvalue())
+
+    def test_do_destroy_error_advanced(self):
+        """Tests destroy() command with errors."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd(".destroy()"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd("garbage.destroy()"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class doesn't exist **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().precmd("BaseModel.destroy()"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** instance id missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(
+                HBNBCommand().precmd('BaseModel.destroy("6524359")'))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** no instance found **")
+
+    def help_load_dict(self, rep):
+        """Helper method to test dictionary equality."""
+        rex = re.compile(r"^\[(.*)\] \((.*)\) (.*)$")
+        res = rex.match(rep)
+        self.assertIsNotNone(res)
+        s = res.group(3)
+        s = re.sub(r"(datetime\.datetime\([^)]*\))", "'\\1'", s)
+        d = json.loads(s.replace("'", '"'))
+        return d
+
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
         from models.base_model import BaseModel
@@ -99,6 +284,44 @@ EOF  all  count  create  destroy  help  quit  show  update
                    "Place": Place,
                    "Review": Review}
         return classes
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+                     {"place_id": str,
+                      "user_id": str,
+                      "text": str}
+        }
+        return attributes
 
 if __name__ == "__main__":
     unittest.main()
