d501f2b47db2af9c3bb4bec2d1abc3de4ccde289
Arthur Damm <indigoarthur@gmail.com>
2019-07-03 22:36:57 +0000

    Add create checks using patch, but also still storage

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for TestHBNBCommand class."""
 
+from models import storage
 from console import HBNBCommand
 import unittest
 from unittest.mock import patch
@@ -49,6 +50,51 @@ EOF  all  count  create  destroy  help  quit  show  update
             self.assertFalse(HBNBCommand().onecmd("                  \n"))
         s = ""
         self.assertEqual(s, f.getvalue())
+
+    def test_do_create(self):
+        """Tests create for all classes."""
+        for classname in self.classes():
+            self.help_test_do_create(classname)
+
+    def help_test_do_create(self, classname):
+        """Helper method to test the create commmand."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create {}".format(classname)))
+        uid = f.getvalue()[:-1]
+        self.assertTrue(len(uid) > 0)
+        key = "{}.{}".format(classname, uid)
+        self.assertTrue(key in storage.all())
+
+    def test_do_create_error(self):
+        """Tests create command with errors."""
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class name missing **")
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.assertFalse(HBNBCommand().onecmd("create garbage"))
+        msg = f.getvalue()[:-1]
+        self.assertEqual(msg, "** class doesn't exist **")
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
 if __name__ == "__main__":
     unittest.main()
